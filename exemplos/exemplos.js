35ae1de15551fc7d948e3b6850c301dbfe797c20
Gabriel archie <gabriel.a.silva24@edu.senai.br>
2026-05-21 15:45:14 -0300

    exemplos

--- exemplos/exemplos.js
+++ exemplos/exemplos.js
@@ -1,21 +1,30 @@
-
-//Crienado arrays com nomes de frutas e sucos
-let sucos = ["Laranja", "Uva", "Abacaxi", "Manga", "Maracujá"];
+//Criando arrays com nomes de frutas e sucos
+let sucos = ['Laranja', 'Abacaxi', 'Maracujá', 'Uva'];
 console.log(sucos[0]);
 
 //Criando um array de números
-let numeros = [1, 2, 3, 4, 5];
-console.log(numeros[2]);
+let números = [1, 2, 3, 4, 5];
+console.log(números[2]);
 
 //Acessando um elemento específico do array
-let animais = ["Cachorro", "Gato", "Pássaro", "Peixe"];
+let animais = ['Cachorro', 'Gato', 'Coelho', 'Hamster'];
 console.log(animais[1]);
 
 //Alterando um elemento do array
-let times = ["Flamengo", "Palmeiras", "São Paulo", "Corinthians"];
-times[2] = "Vasco";
+let times = ['Palmeiras', 'Flamengo', 'São Paulo', 'Corinthians'];
+times[2] = 'Grêmio';
 console.log(times);
 
 //Descobrindo o tamanho do array
-let cores = ["Vermelho", "Verde", "Azul", "Amarelo"];
-console.log(cores.length); // Nota: Em JavaScript, o correto é "length" com "l" minúsculo.
+let games = ['FIFA', 'Call of Duty', 'Minecraft', 'Fortnite'];
+console.log(games.length);
+
+//Adicionando um elemento ao final do array
+let cores = ['vermelho', 'azul', 'verde'];
+cores.push('amarelo');
+console.log(cores);
+
+//Removendo o último elemento do array
+let carros = ['Fusca', 'Gol', 'Civic', 'Corolla'];
+carros.pop();
+console.log(carros);
