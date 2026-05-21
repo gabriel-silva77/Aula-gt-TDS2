1544b301369ff19a58e64677a83c0f628bfa9537
Gabriel archie <gabriel.a.silva24@edu.senai.br>
2026-05-21 14:38:14 -0300

    exemplos array

---
+++ exemplos/exemplos.js
@@ -0,0 +1,21 @@
+
+//Crienado arrays com nomes de frutas e sucos
+let sucos = ["Laranja", "Uva", "Abacaxi", "Manga", "Maracujá"];
+console.log(sucos[0]);
+
+//Criando um array de números
+let numeros = [1, 2, 3, 4, 5];
+console.log(numeros[2]);
+
+//Acessando um elemento específico do array
+let animais = ["Cachorro", "Gato", "Pássaro", "Peixe"];
+console.log(animais[1]);
+
+//Alterando um elemento do array
+let times = ["Flamengo", "Palmeiras", "São Paulo", "Corinthians"];
+times[2] = "Vasco";
+console.log(times);
+
+//Descobrindo o tamanho do array
+let cores = ["Vermelho", "Verde", "Azul", "Amarelo"];
+console.log(cores.length); // Nota: Em JavaScript, o correto é "length" com "l" minúsculo.
